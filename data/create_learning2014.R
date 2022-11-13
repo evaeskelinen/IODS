b724837a26fd4ea2910f90b45dc9ede0b2cbaf89
Eva Eskelinen <eva.eskelinen@helsinki.fi>
2022-11-13 15:57:50 +0200

    A test run.

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -1,2 +1,6 @@
-#Assignment 2  
-Eva Eskelinen, 12.11.2022 
+#Assignment 2  Eva Eskelinen, 12.11.2022 
+# Reading the full data from learning2014 was a struggle and I'm still not sure if I did it correctly
+# to read the data use: lrn <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+# I am not quite sure what the purpose of 'sep\t", header=TRUE' is in this function, but I guess it will be clearer at some point. This formula was from the exercise 2 sheet.
+> View(learning2015)
+
